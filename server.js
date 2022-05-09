c167a668f5103c8b4289d5d8600a63741befd875
Sherreka <rekadarnb@gmail.com>
2022-05-09 11:19:00 -0400

    added post info

--- server.js
+++ server.js
@@ -28,6 +28,35 @@ app.get('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'db/db.json'))
 );
 
+//Post info to the notes file
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
 
 //Set up port to listen to requests
 app.listen(PORT, () =>
