0f286a4b95fb413e8091cd14d8111e58db41a1f8
Sherreka <rekadarnb@gmail.com>
2022-05-09 11:11:49 -0400

    added comments to code

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
+//Get modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+
+//Use express middleware
 const app = express(); 
 const PORT = process.env.port || 3001;
 
+//How to handle data
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-
-app.get('*', (req, res) =>
+//Get home page
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+// Get notes page 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+//Get notes info
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'db/db.json'))
+);
+
+
+//Set up port to listen to requests
 app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Note app listening at http://localhost:${PORT}`)
 );
