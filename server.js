ec87dada3a0557b49bfda99f3e5a394769662d50
Sherreka <rekadarnb@gmail.com>
2022-05-11 13:04:08 -0400

    changed port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const notes = require('./db/db.json')
 
 //Use express middleware
 const app = express(); 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3000;
 
 //Make unique ID
 let uuid = new ShortUniqueId({ length: 4});
